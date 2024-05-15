fff74c7e54e47caa96622908ecb2858f7922db7c
imsuck <imsuck12@gmail.com>
2024-05-15 16:49:50 +0700

    Update testing scripts

--- script/compile.sh
+++ script/compile.sh
@@ -7,10 +7,13 @@ DIR="$(dirname "$0")"
 
 echo
 echo "---------- Compilation ---------"
+start=`date +%s.%N`
 g++ -include bits/stdc++.h -include lib/debug.h -std=c++14 -DLOCAL \
 -Wall -Wextra -Wconversion -Wshadow -Wfloat-equal \
 `# -O2` \
 `# -fsanitize=undefined -fsanitize=address` \
 -o a.out $(fd -ae cpp --changed-within 3s)
+end=`date +%s.%N`
+echo "Compilation took: $(echo "$end - $start" | bc -l)s"
 
 bash -c "$DIR/test.sh"
--- script/test.sh
+++ script/test.sh
@@ -5,4 +5,5 @@ echo "------------- Test -------------"
 start=`date +%s.%N`
 cat "a.inp" | ./a.out
 end=`date +%s.%N`
+echo
 echo "Time: $(echo "$end - $start" | bc -l)s"
