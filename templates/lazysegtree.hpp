96731d5bab7b0a8d164ccee835de483349746f27
imsuck <imsuck12@gmail.com>
2024-07-21 22:50:33 +0700

    Fix: `lazysegtree.hpp` crashes under certain sizes Info: When using O(2n) implementation the segment tree could crash with `n = 200_000` using power-of-two sizes prevents that

--- templates/lazysegtree.hpp
+++ templates/lazysegtree.hpp
@@ -2,38 +2,38 @@
 #include <vector>
 using namespace std;
 
-// M: S M::e, F M::id, S op(S, S), F comp(F, F), S map(F, S)
+// M: S M::e, F M::id, S op(S, S), F comp(F, F), bool map(F, &S)
 // clang-format off
 #pragma GCC diagnostic ignored "-Wconversion"
 template<class M, class S = typename M::S, class F = typename M::F>
 struct LazySegTree {
-  public:
-    LazySegTree() : LazySegTree(0) {}
-    LazySegTree(int _n) : n(_n), t(n * 2, M::e), upd(n, false), lz(n, M::id) {
+    LazySegTree() = default;
+    LazySegTree(int _n) : n(_n) {
         while (1 << lg < n) lg++;
+        m = 1 << lg;
+        t.assign(m * 2, M::e), upd.assign(m, false), lz.assign(m, M::id);
     }
     LazySegTree(const vector<S> &v) : LazySegTree(v.size()) {
-        copy(begin(v), end(v), begin(t) + n);
-        for (int i = n - 1; i > 0; i--) update(i);
+        copy(begin(v), end(v), begin(t) + m);
+        for (int i = m - 1; i > 0; i--) update(i);
     }
 
     void set(int p, const S &x) {
         assert(0 <= p && p < n);
-        push_to(p), t[p + n] = x, update_from(p);
+        push_to(p), t[p + m] = x, update_from(p);
     }
     S get(int p) {
         assert(0 <= p && p < n);
-        push_to(p);
-        return t[p + n];
+        return push_to(p), t[p + m];
     }
 
-    S all_prod() { return prod(0, n); }
+    S all_prod() { return t[1]; }
     S prod(int l, int r) {
         assert(0 <= l && l <= r && r <= n);
         if (l == r) return M::e;
-        push_to(l, r);
+        push_to(l), push_to(r);
         S sml = M::e, smr = M::e;
-        for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+        for (l += m, r += m; l < r; l >>= 1, r >>= 1) {
             if (l & 1) sml = M::op(sml, t[l++]);
             if (r & 1) smr = M::op(t[--r], smr);
         }
@@ -42,38 +42,37 @@ struct LazySegTree {
 
     void apply(int p, const F &f) {
         assert(0 <= p && p < n);
-        push_to(p), t[p + n] = M::map(f, t[p + n]), update_from(p);
+        push_to(p), t[p + m] = M::map(f, t[p + m]), update_from(p);
     }
     void apply(int l, int r, const F &f) {
         assert(0 <= l && l <= r && r <= n);
         if (l == r) return;
-        push_to(l, r);
-        for (int l2 = l + n, r2 = r + n; l2 < r2; l2 >>= 1, r2 >>= 1) {
+        push_to(l), push_to(r);
+        for (int l2 = l += m, r2 = r += m; l2 < r2; l2 >>= 1, r2 >>= 1) {
             if (l2 & 1) all_apply(l2++, f);
             if (r2 & 1) all_apply(--r2, f);
         }
-        update_from(l, r);
+        int li = __builtin_ctz(l), ri = __builtin_ctz(r);
+        for (int i = li + 1; i <= lg; i++) update(l >> i);
+        for (int i = ri + 1; i <= lg; i++) update(r >> i);
     }
 
   private:
-    int n, lg = 0;
+    int n, m, lg = 0;
     vector<S> t;
     vector<bool> upd;
     vector<F> lz;
     void update(int p) { t[p] = M::op(t[p << 1], t[p << 1 | 1]); }
     void update_from(int p) {
-        p += n;
+        p += m;
         for (int i = 1; i <= lg; i++) update(p >> i);
     }
-    void update_from(int l, int r) {
-        l += n, r += n;
-        int li = __builtin_ctz(l), ri = __builtin_ctz(r);
-        for (int i = li + 1; i <= lg; i++) update(l >> i);
-        for (int i = ri + 1; i <= lg; i++) update(r >> i);
-    }
     void all_apply(int p, const F &f) {
-        t[p] = M::map(f, t[p]);
-        if (p < n) lz[p] = M::comp(f, lz[p]), upd[p] = true;
+        bool ok = M::map(f, t[p]);
+        if (p < m) {
+            lz[p] = M::comp(f, lz[p]), upd[p] = true;
+            if (!ok) push(p), update(p);
+        }
     }
     void push(int p) {
         if (!upd[p]) return;
@@ -81,14 +80,8 @@ struct LazySegTree {
         lz[p] = M::id, upd[p] = false;
     }
     void push_to(int p) {
-        p += n;
+        p += m;
         for (int i = lg; i >= 1; i--) push(p >> i);
-    }
-    void push_to(int l, int r) {
-        l += n, r += n;
-        int li = __builtin_ctz(l), ri = __builtin_ctz(r);
-        for (int i = lg; i >= li + 1; i--) push(l >> i);
-        for (int i = lg; i >= ri + 1; i--) push(r >> i);
     }
 };
 #pragma GCC diagnostic warning "-Wconversion"
