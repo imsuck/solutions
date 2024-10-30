9c3e01c0c8792574c315a39d0d3954331601489e
imsuck <imsuck12@gmail.com>
2024-10-30 17:36:37 +0700

    Chinese adjacency list idk

--- templates/csr.hpp
+++ templates/csr.hpp
@@ -1,19 +1,20 @@
 #include <vector>
 using namespace std;
 
-template<class E> struct csr {
-    csr(int _n, const vector<pair<int, E>> &edges)
-        : n(_n), start(n + 1), elist(edges.size()) {
+template<class E> struct CSR {
+    CSR() {}
+    CSR(int _n, const vector<pair<int, E>> &edges)
+        : n(_n), start(n + 1), edges(edges.size()) {
         for (auto &[from, _] : edges) start[from + 1]++;
         for (int i = 1; i <= n; i++) start[i] += start[i - 1];
         auto cnt = start;
-        for (auto &[from, e] : edges) elist[cnt[from]++] = e;
+        for (auto &[from, e] : edges) edges[cnt[from]++] = e;
     }
 
     int size() const { return n; }
 
     struct range {
-        E *elist;
+        E *edges;
         const int first, last;
         struct iterator {
             E *elist;
@@ -24,16 +25,16 @@ template<class E> struct csr {
         };
         E &operator[](size_t i) {
             assert(i < size());
-            return elist[first + i];
+            return edges[first + i];
         }
-        E &back() { return elist[last - 1]; }
-        iterator begin() { return {elist, first}; }
-        iterator end() { return {elist, last}; }
+        E &back() { return edges[last - 1]; }
+        iterator begin() { return {edges, first}; }
+        iterator end() { return {edges, last}; }
         int size() const { return last - first; }
         bool empty() const { return first == last; }
     };
     struct const_range {
-        const E *elist;
+        const E *edges;
         const int first, last;
         struct iterator {
             const E *elist;
@@ -44,32 +45,32 @@ template<class E> struct csr {
         };
         const E &operator[](size_t i) const {
             assert(i < size());
-            return elist[first + i];
+            return edges[first + i];
         }
-        const E &back() const { return elist[last - 1]; }
-        iterator begin() const { return {elist, first}; }
-        iterator end() const { return {elist, last}; }
+        const E &back() const { return edges[last - 1]; }
+        iterator begin() const { return {edges, first}; }
+        iterator end() const { return {edges, last}; }
         int size() const { return last - first; }
         bool empty() const { return first == last; }
     };
     range operator[](size_t i) {
         assert(i < size());
-        return {elist.data(), start[i], start[i + 1]};
+        return {edges.data(), start[i], start[i + 1]};
     }
     const_range operator[](size_t i) const {
         assert(i < size());
-        return {elist.data(), start[i], start[i + 1]};
+        return {edges.data(), start[i], start[i + 1]};
     }
 
     struct iterator {
-        csr *g;
+        CSR *g;
         int p;
         bool operator!=(const iterator &r) const { return p != r.p; }
         void operator++() { p++; }
         range operator*() { return (*g)[p]; }
     };
     struct const_iterator {
-        const csr *g;
+        const CSR *g;
         int p;
         bool operator!=(const const_iterator &r) const { return p != r.p; }
         void operator++() { p++; }
@@ -83,5 +84,5 @@ template<class E> struct csr {
   private:
     int n;
     vector<int> start;
-    vector<E> elist;
+    vector<E> edges;
 };
