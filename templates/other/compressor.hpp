75a800fe51ef1a5c21bd314c92afc55ca686ba19
imsuck <imsuck12@gmail.com>
2025-05-21 20:27:30 +0700

    update compressor.hpp

--- templates/other/compressor.hpp
+++ templates/other/compressor.hpp
@@ -1,15 +1,16 @@
+#include <limits>
 #include <vector>
 using namespace std;
 
-template<class T> struct Compressor {
+template<class T = int> struct Compressor {
     vector<reference_wrapper<T>> val;
     vector<T> og;
 
-    void push(T &x) { val.push_back(x); }
+    template<class... Ts> void push(Ts &...x) { (_push(x), ...); }
     void build() {
         sort(begin(val), end(val));
         og.reserve(val.size());
-        T id = -1, prv = -1;
+        T id = -1, prv = numeric_limits<T>::max();
         for (auto &x : val) {
             if (prv != x) {
                 id++, prv = x;
@@ -21,4 +22,13 @@ template<class T> struct Compressor {
     int size() const { return int(og.size()); }
     T operator[](int i) const { return og[i]; }
     bool find(T x) const { return binary_search(begin(og), end(og), x); }
+
+  private:
+    void _push(T &x) { val.push_back(x); }
+    void _push(pair<T, T> &p) {
+        val.push_back(p.first), val.push_back(p.second);
+    }
+    template<class V> void _push(V &a) {
+        for (auto &x : a) _push(x);
+    }
 };
