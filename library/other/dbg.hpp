766156d95b073301d872ce566ccbcb7479f27a1f
imsuck <imsuck12@gmail.com>
2025-09-18 23:07:08 +0700

    move library to git submodule

--- library/other/dbg.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef LOCAL
-    #define dbg(...) void()
-    #define debug() if (false)
-#else
-namespace dbg::options {
-    template<> constexpr bool trivial_string() { return true; }
-} // namespace dbg::options
-#endif
--- library/other/st_alloc.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-template<class T> struct st_alloc {
-    static inline stack<T> pool;
-    void *operator new(size_t) { return pool.emplace(), &pool.top(); }
-    void operator delete(void *) {}
-};
